a48889e6ae84c37783ca5d88e42e4b4d3d739cca
Ahmad Z. Ihsan <a.ihsan@fz-juelich.de>
2023-08-23 09:49:17 +0200

    feature:use case 1

---
+++ use-cases/UC1/python-script/map_data.py
@@ -0,0 +1,24 @@
+import json
+import os
+from rdflib import Graph
+from rdf_serializer import rdf_serializer
+from pathlib import Path
+
+# Load data 
+path_data = Path('../data/MaterialProject/mp_Aluminium') #mp_Aluminium, mp_Copper, mp_Nickel, mp_Tungsten, mp_Zirconium
+
+def main():
+    path_cif_json = path_data/'json'/'Al_cif.json' #Al_cif.json, Cu_cif.json, Ni_cif.json, W_cif.json, Zr_cif.json
+    path_sg = path_data/'spacegroup'/'Al_spacegroup.json' #Al_spacegroup.json, Cu_spacegroup.json, Ni_spacegroup.json, W_spacegroup.json, Zr_spacegroup.json
+    with open(path_cif_json) as data1, open(path_sg) as data2: 
+        cif_data = json.load(data1)
+        sg_data = json.load(data2)
+        mat_id = path_cif_json.stem 
+        save_file = path_cif_json.stem +'.ttl'
+        path_save = Path('../turtle/')/save_file
+        example_uri = "http://www.example.org/{}/".format(mat_id)
+        g = rdf_serializer(cif_data, sg_data, example_uri, mat_id)
+        g.serialize(destination=path_save, format='turtle')
+
+if __name__ == "__main__":
+    main()
---
+++ use-cases/UC1/python-script/rdf_serializer.py
@@ -0,0 +1,98 @@
+from rdflib import Graph, Literal
+from rdflib.namespace import  Namespace, RDF, XSD
+
+
+#crystal structure ontology
+CSO = Namespace("https://purls.helmholtz-metadaten.de/disos/cso#")
+MDO = Namespace("https://w3id.org/mdo/structure/")
+CHEBI = Namespace("http://purl.obolibrary.org/obo/")
+CHEBIdat = Namespace("http://purl.obolibrary.org/obo/chebi/")
+
+# function to serializing the CIF and space group data into the Resource Description Framework (RDF) using the CSO
+# returning graph object of rdflib
+def rdf_serializer(cif_dict, space_group, uri, mat_id):
+    #Material data URI
+    example = Namespace(uri)
+    g = Graph()
+    g.parse("../../../CSO/crystal-structure-ontology.owl", format="xml")
+    space_group_data = space_group['spacegroup']
+    Bravais_lattice = example['{}_Bravais_lattice'.format(mat_id)]
+    crystal_structure = example['{}_crystal_strcture'.format(mat_id)]
+    crystal_system = example['{}_crystal_system'.format(mat_id)]
+    unit_cell = example['{}_unit_cell'.format(mat_id)]
+    occupancy = example['{}_occupancy'.format(mat_id)]
+    atom = example['{}_atom'.format(mat_id)]
+    lattice_param_length = example['{}_lattice_parameter_length'.format(mat_id)]
+    lattice_param_angle = example['{}_lattice_parameter_angle'.format(mat_id)]
+    space_group = example['{}_space_group'.format(mat_id)]
+    point_group = example['{}_point_group'.format(mat_id)]
+    
+    ## length data value
+    length_a = Literal(cif_dict['_cell_length_a']*1e-10, datatype=XSD.double) 
+    length_b = Literal(cif_dict['_cell_length_b']*1e-10, datatype=XSD.double)
+    length_c = Literal(cif_dict['_cell_length_c']*1e-10, datatype=XSD.double)
+    
+    ##angle data value
+    angle_alpha = Literal(cif_dict['_cell_angle_alpha'], datatype=XSD.double)  
+    angle_beta =  Literal(cif_dict['_cell_angle_beta'], datatype=XSD.double)
+    angle_gamma = Literal(cif_dict['_cell_angle_gamma'], datatype=XSD.double)
+    
+    ## chemical structural formula
+    dat_atom = Literal(cif_dict['_chemical_formula_structural'], datatype=XSD.string)
+    
+    
+    #binding alias for namespace in rdf graph
+    g.bind("ex", example)
+    g.bind("cso", CSO)
+    g.bind("mdo", MDO)
+    g.bind("chebi", CHEBI)
+    
+    g.add((crystal_structure, RDF.type, CSO.CrystalStructure))
+    g.add((crystal_structure,CSO.hasLattice, Bravais_lattice))
+    g.add((crystal_structure, MDO.hasSpaceGroup, space_group))
+    g.add((space_group, RDF.type, MDO.SpaceGroup))
+    g.add((space_group, MDO.SpaceGroupID, Literal(space_group_data['number'], datatype=XSD.integer)))
+    g.add((space_group, MDO.SpaceGroupSymbol, Literal(space_group_data['symbol'], datatype=XSD.string)))
+    g.add((space_group, MDO.hasPointGroup, point_group))
+    g.add((point_group, RDF.type, MDO.PointGroup))
+    g.add((point_group, MDO.PointGroupHMName, Literal(space_group_data['point_group'], datatype=XSD.string)))
+    g.add((Bravais_lattice, RDF.type, CSO.BravaisLattice))
+    g.add((Bravais_lattice, CSO.centering, Literal(space_group_data['symbol'][0], datatype=XSD.string)))
+    g.add((Bravais_lattice, CSO.hasCrystalSystem, crystal_system))
+    g.add((Bravais_lattice, CSO.hasUnitCell, unit_cell))
+    g.add((crystal_system, RDF.type, CSO[space_group_data['crystal_system'].capitalize()]))
+    g.add((point_group, CSO.isPointGroupOf, crystal_system))
+    g.add((unit_cell, RDF.type, CSO.UnitCell))
+    g.add((unit_cell, CSO.hasLatticeParameterLength, lattice_param_length))
+    g.add((unit_cell, CSO.hasLatticeParameterAngle, lattice_param_angle))
+    g.add((lattice_param_length, RDF.type, CSO.LatticeParameterLength))
+    g.add((lattice_param_angle, RDF.type, CSO.LatticeParameterAngle))
+    g.add((lattice_param_length, CSO.latticeParameterLengthA , length_a))
+    g.add((lattice_param_length, CSO.latticeParameterLengthB, length_b))
+    g.add((lattice_param_length, CSO.latticeParameterLengthC, length_c))
+    g.add((lattice_param_angle, CSO.latticeParameterAngleAlpha, angle_alpha))
+    g.add((lattice_param_angle, CSO.latticeParameterAngleBeta, angle_beta))
+    g.add((lattice_param_angle, CSO.latticeParameterAngleGamma, angle_gamma))
+    
+    #Occupancy
+    for i in range(len(cif_dict['_atom_site_occupancy'])):
+        occupancy=example['{}_occupancy_{}'.format(mat_id, i)]
+        atom = example['{}_atom_{}'.format(mat_id,i)]
+        site = example['{}_site_{}'.format(mat_id,i)]
+        frac_coord = example['{}_frac_coord_{}'.format(mat_id,i)]
+        g.add((crystal_structure, MDO.hasOccupancy, example['{}_occupancy_{}'.format(mat_id,i)]))
+        g.add((example['{}_occupancy_{}'.format(mat_id,i)], RDF.type, MDO.Occupancy))
+        g.add((example['{}_occupancy_{}'.format(mat_id,i)], MDO.hasSpecies, example['{}_species_{}'.format(mat_id,i)]))
+        g.add((example['{}_species_{}'.format(mat_id,i)], RDF.type, MDO.Species))
+        g.add((example['{}_species_{}'.format(mat_id,i)], MDO.hasElement, atom))
+        g.add((atom, RDF.type, CHEBI.CHEBI_33250))
+        g.add((atom, CHEBIdat.formula, dat_atom))
+        g.add((occupancy, MDO.hasSite, site))
+        g.add((site, RDF.type, MDO.Site))
+        g.add((site, MDO.hasFractionalCoordinates, frac_coord))
+        g.add((frac_coord, RDF.type, MDO.CoordinateVector))
+        g.add((frac_coord, MDO.X_axisCoordinate, Literal(cif_dict['_atom_site_fract_x'][i], datatype=XSD.double)))
+        g.add((frac_coord, MDO.Y_axisCoordinate, Literal(cif_dict['_atom_site_fract_y'][i], datatype=XSD.double)))
+        g.add((frac_coord, MDO.Z_axisCoordinate, Literal(cif_dict['_atom_site_fract_z'][i], datatype=XSD.double))) 
+
+    return g
